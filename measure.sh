63a0854ccbc73dc4663ab8d3b597b38788db935c
Stefano <stefano.forti92@gmail.com>
2016-02-17 19:12:23 +0100

    measureAll.sh corrected

--- measure.sh
+++ measure.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-./measureAll.sh "seq" 481183 500 18
+./measureAll.sh "seq" 481183 500 1
 ./measureAll.sh "mt" 481183 500 18
 ./measureAll.sh "sk" 481183 500 18
 ./measureAll.sh "mu2" 481183 500 18
 
-./measureAll.sh "seq" 481183 1000 18
+./measureAll.sh "seq" 481183 1000 1
 ./measureAll.sh "mt" 481183 1000 18
 ./measureAll.sh "sk" 481183 1000 18
 ./measureAll.sh "mu2" 481183 1000 18
 
-./measureAll.sh "seq" 481183 2000 18
+./measureAll.sh "seq" 481183 2000 1
 ./measureAll.sh "mt" 481183 2000 18
 ./measureAll.sh "sk" 481183 2000 18
 ./measureAll.sh "mu2" 481183 2000 18
 
-./measureAll.sh "seq" 481183 4000 18
+./measureAll.sh "seq" 481183 4000 1
 ./measureAll.sh "mt" 481183 4000 18
 ./measureAll.sh "sk" 481183 4000 18
 ./measureAll.sh "mu2" 481183 4000 18
 
-./measureAll.sh "seq" 481183 8000 18
+./measureAll.sh "seq" 481183 8000 1
 ./measureAll.sh "mt" 481183 8000 18
 ./measureAll.sh "sk" 481183 8000 18
 ./measureAll.sh "mu2" 481183 8000 18
 
-./measureAll.sh "seq" 481183 10000 18
+./measureAll.sh "seq" 481183 10000 1
 ./measureAll.sh "mt" 481183 10000 18
 ./measureAll.sh "sk" 481183 10000 18
 ./measureAll.sh "mu2" 481183 10000 18
