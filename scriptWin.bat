9c2aeb4371b2d2321f6ea180fb2b40abcbbf04b6
Stefano <stefano.forti92@gmail.com>
2016-02-20 13:45:48 +0100

    windows script

---
+++ scriptWin.bat
@@ -0,0 +1,4 @@
+mvn install:install-file "-Dfile=.\lib\muskel-core-1.0.0.jar" "-DgroupId=it.reactive.muskel" "-DartifactId=muskel-core" "-Dversion=1.0.0" "-Dpackaging=jar";
+mvn install:install-file "-Dfile=.\\lib\\skandium-1.0.0.jar" "-DgroupId=cl.niclabs" "-DartifactId=skandium" "-Dversion=1.0.0" "-Dpackaging=jar";
+
+mvn install
